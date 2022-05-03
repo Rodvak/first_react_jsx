574efb5512778b2e5f99e10373d408cf64b764ed
Alex <av.rodriguez@outlook.com>
2022-05-03 16:37:30 -0700

    commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Container extends Component {
+  render() {
+    return (
+      <div>I am the Container file.</div>
+    )
+  }
+}
+
+export default Container
